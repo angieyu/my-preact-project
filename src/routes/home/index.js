27515dd9a7e8fe5a4d09b05b34249bf1b6ae5751
Angela Yu <angelayu@Angelas-MacBook-Pro.local>
2019-10-07 17:38:26 -0700

    finished preact tutorial

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,34 @@
-import { h } from 'preact';
+import { render, Component } from 'preact';
 import style from './style';
+import Clock from './clock'
 
-const Home = () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-	</div>
-);
+export default class Home extends Component {
+	state = {
+		value: '',
+		name: 'world'
+	}
 
-export default Home;
+	onInput = ev => {
+		this.setState({ value: ev.target.value });
+	}
+
+	onSubmit = (e) => {
+		e.preventDefault();
+		this.setState({ name: this.state.value })
+	}
+
+	render() {
+		return (
+			<div class={style.home}>
+				<h1>Landing page</h1>
+				<p>Hello {this.state.name}!</p>
+				<form onSubmit={this.onSubmit}>
+					<input type="text" value={this.state.value} onInput={this.onInput} />
+					<button type="submit">Update</button>
+				</form>
+				<br/>
+				<Clock />
+			</div>
+		)
+	}
+}
